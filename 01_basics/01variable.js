e3720ee322cbdc56f0b5936654c1f1069f42ed29
DHIRAJ KUMAR JHA <142652872+01Dhiraj@users.noreply.github.com>
2024-06-04 17:04:09 +0000

    variables

---
+++ 01_basics/01variable.js
@@ -0,0 +1,13 @@
+const accountId=12345
+let accountEmail="dhirajjha7870@gmail.com"
+var accountpassword="12345"
+accountcity="darbhanga"
+
+accountEmail="dk@gmail.com"
+accountpassword="675675"
+accountcity="mumbai"
+let accountstate;
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate])
