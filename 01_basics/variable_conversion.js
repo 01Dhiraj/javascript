ecb01ea120f554ef4c65cc24454c030f34b7ab91
DHIRAJ KUMAR JHA <142652872+01Dhiraj@users.noreply.github.com>
2024-06-08 11:07:56 +0000

    Dataconversion

---
+++ 01_basics/variable_conversion.js
@@ -0,0 +1,15 @@
+let score="33"
+
+console.log(typeof (score))
+let valueinnumber=Number(score)
+console.log(typeof(valueinNumber))
+console.log(valueinnumber)
+// 33=>number
+// "33"=>string
+
+let isloggedin="dhiraj"
+let booleanisloggedin=Boolean(isloggedin)
+console.log(booleanisloggedin)
+//1=>true; 0=>false
+//""=>false
+//"dhiraj"=>true
